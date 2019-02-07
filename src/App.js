3891d1e3d7fb3c1eaed2bbed231c1b00d906f87b
Unknown <riderjensen@gmail.com>
2019-02-07 13:58:51 -0700

    Add event changes

--- src/App.js
+++ src/App.js
@@ -17,11 +17,26 @@ class App extends Component {
 		]
 	}
 
-	switchNameHandler = () => {
+	switchNameHandler = (newName) => {
 		this.setState({
 			persons: [
 				{
-					name: 'Riderino',
+					name: newName,
+					age: 26
+				},
+				{
+					name: 'Zooop',
+					age: 200
+				}
+			]
+		})
+	}
+
+	nameChangedHandler = (event) => {
+		this.setState({
+			persons: [
+				{
+					name: event.target.value,
 					age: 26
 				},
 				{
@@ -36,9 +51,18 @@ class App extends Component {
 		return (
 			<div className="App">
 				<h1>I am a react app</h1>
-				<button onClick={this.switchNameHandler}>Change name</button>
-				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Zoop boop shloop</Person>
-				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Scoop whoop</Person>
+				<button onClick={() => this.switchNameHandler('Zoop boop')}>Change name</button>
+				<Person
+					name={this.state.persons[0].name}
+					age={this.state.persons[0].age}
+					click={this.switchNameHandler.bind(this, 'Ridont')}
+					changed={this.nameChangedHandler}
+
+				>Zoop boop shloop</Person>
+				<Person
+					name={this.state.persons[1].name}
+					age={this.state.persons[1].age}
+				>Scoop whoop</Person>
 
 			</div>
 		);
