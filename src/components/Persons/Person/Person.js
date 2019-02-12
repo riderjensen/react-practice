04f8b0f851a8927cbfbed786a4af2ad7f0a75dc7
Unknown <riderjensen@gmail.com>
2019-02-12 13:35:34 -0700

    Using react fragment instead of aux

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Auxillary';
 import classes from './Person.css';
 
 class Person extends Component {
 	render() {
 		return (
-			<Aux>
+			<React.Fragment>
 				<div className={classes.Person} >
 					<p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
 					<input type="text" onChange={this.props.changed} value={this.props.name} />
 				</div>
-			</Aux>
+			</React.Fragment>
 		)
 	}
 
