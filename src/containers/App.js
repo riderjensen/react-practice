2c8f21b9de651f575f6b83ac9a635a6548d90841
Unknown <riderjensen@gmail.com>
2019-02-11 18:18:18 -0700

    Add two new component hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -61,7 +61,15 @@ class App extends Component {
 
 	componentDidMount() {
 		console.log('[App.js] componentDidMount');
+	}
 
+	shouldComponentUpdate(nextProps, nextState) {
+		console.log('[App.js] shouldComponentUpdate');
+		return true;
+	}
+
+	componentDidUpdate() {
+		console.log('[App.js] componentDidUpdate');
 	}
 
 	render() {
