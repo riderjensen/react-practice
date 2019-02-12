4da39af506fed28c4a61e784adb73d9d1e09d8b8
Unknown <riderjensen@gmail.com>
2019-02-12 13:52:03 -0700

    Using wraping function from withClass

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 
 import classes from './Person.css';
+import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<div className={classes.Person} >
 					<p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
 					<input type="text" onChange={this.props.changed} value={this.props.name} />
-				</div>
 			</React.Fragment>
 		)
 	}
 
 }
 
-export default Person;
+export default withClass(Person, classes.person);
