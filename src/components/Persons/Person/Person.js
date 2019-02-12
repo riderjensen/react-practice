18ac876fc5494b03f71defddf84364d05635854b
Unknown <riderjensen@gmail.com>
2019-02-12 14:18:06 -0700

    Two different ways to use reference

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -5,11 +5,23 @@ import classes from './Person.css';
 import withClass from '../../../hoc/withClass';
 
 class Person extends Component {
+	constructor(props) {
+		super(props);
+		this.myElementRef =  React.createRef();
+	}
+
+	componentDidMount(){
+		this.myElementRef.current.focus()
+	}
+
 	render() {
 		return (
 			<React.Fragment>
 					<p onClick={this.props.click}>I am {this.props.name} and I am {this.props.age} years old</p>
-					<input type="text" onChange={this.props.changed} value={this.props.name} />
+					<input type="text" onChange={this.props.changed} value={this.props.name} 
+					// ref={(thisElement) => {this.myElement = thisElement}}
+					ref={this.myElementRef}
+					/>
 			</React.Fragment>
 		)
 	}
