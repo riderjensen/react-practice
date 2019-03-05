8d888afa5cf9c47bf7869e8472a272c053f58d71
Unknown <riderjensen@gmail.com>
2019-03-05 14:43:24 -0700

    Using utility function for returning state

--- counter/src/store/reducers/result.js
+++ counter/src/store/reducers/result.js
@@ -1,22 +1,20 @@
 import * as actionTypes from '../actions/actionTypes';
-
+import { updateObject } from '../utility';
 const initialState = {
 	results: []
+}
+
+const deleteResult = (state, action) => {
+	const updatedArray = state.results.filter((result, index) => result.id !== action.resultElId);
+	return updateObject(state, { results: updatedArray })
 }
 
 export default function (state = initialState, action) {
 	switch (action.type) {
 		case actionTypes.STORE_RESULT:
-			return {
-				...state,
-				results: state.results.concat({ id: new Date(), value: action.result })
-			}
+			return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) })
 		case actionTypes.DELETE_RESULT:
-			const updatedArray = state.results.filter((result, index) => result.id !== action.resultElId);
-			return {
-				...state,
-				results: updatedArray
-			}
+			return deleteResult(state, action);
 		default:
 			return state;
 	}
