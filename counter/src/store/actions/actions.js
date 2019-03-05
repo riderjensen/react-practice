abe0405e485671226505141e3bb468928717354e
Unknown <riderjensen@gmail.com>
2019-03-05 14:01:22 -0700

    Insert action for async code

--- counter/src/store/actions/actions.js
+++ counter/src/store/actions/actions.js
@@ -32,11 +32,21 @@ export const sub = (value) => {
 	};
 };
 
-export const storeResult = (result) => {
+export const saveResult = (result) => {
 	return {
 		type: STORE_RESULT,
 		result: result
 	};
+}
+
+export const storeResult = (result) => {
+	return dispatch => {
+		setTimeout(() => {
+			dispatch(saveResult(result));
+		}, 2000);
+	}
+
+
 };
 
 export const deleteResult = (resElId) => {
